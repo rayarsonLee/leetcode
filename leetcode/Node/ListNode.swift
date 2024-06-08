f38ec5d7048834e5026beab1018e5741c02ac88b
lijiaxin.2333 <lijiaxin.2333@bytedance.com>
2024-06-08 16:57:07 +0800

    这是一个Leetcode刷题的swift代码模板

---
+++ leetcode/Node/ListNode.swift
@@ -0,0 +1,28 @@
+//
+//  ListNode.swift
+//  leetcode
+//
+//  Created by lijiaxin on 2024/5/22.
+//
+
+import Foundation
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+
+extension ListNode: Equatable, Hashable {
+    public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
+        return lhs === rhs
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
+
---
+++ leetcode/Node/TreeNode.swift
@@ -0,0 +1,31 @@
+//
+//  TreeNode.swift
+//  leetcode
+//
+//  Created by lijiaxin on 2024/5/31.
+//
+
+import Foundation
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+extension TreeNode: Equatable, Hashable {
+    public static func == (lhs: TreeNode, rhs: TreeNode) -> Bool {
+        return lhs === rhs
+    }
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+}
+
